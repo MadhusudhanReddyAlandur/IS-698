1004ba9668466b71da74928ce69b68ee6085d5d5
MadhusudhanReddyAlandur <madhusa1@umbc.edu>
2025-04-03 16:49:04 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,12 +2,44 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-04aa00acb1165b32a"
-  instance_type = "t2.micro"
-  key_name      = "MyKeyPair"
-
-  tags = {
-    Name = "Terraform-EC2-Instance"
+# Step 1: Define variable for instance types
+variable "instances" {
+  type = map(string)
+  default = {
+    "web1" = "t2.micro"
+    "web2" = "t3.micro"
+    "web3" = "t2.small"
   }
 }
+
+# Step 2: Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+# Step 3: Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "terraform-subnet"
+  }
+}
+
+# Step 4: Launch EC2 Instances with for_each
+resource "aws_instance" "web" {
+  for_each      = var.instances
+  ami           = "ami-0a9a48ce4458e384e"
+  instance_type = each.value
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = each.key
+  }
+}
+
